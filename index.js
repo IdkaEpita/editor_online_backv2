be9940df93e43bcd3ee78997fe62b6ba1574f2ce
Arhtur Laurent <arthur.laurent@epita.fr>
2020-11-25 10:23:58 -0800

    ADD: mongodb connection

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
+const MongoClient = require('mongodb').MongoClient;
+connectionString = "mongodb+srv://back:Lumos35@nlpf.t3ynp.mongodb.net/nlpf?retryWrites=true&w=majority"
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+  })
+  .catch(error => console.error(error))
+
 server.listen(port, () => {
     console.log('Server listening at port %d', port);
   });
